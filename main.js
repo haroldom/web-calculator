0cb45f2db2c9aa49819174ff392637510c04160e
Harold Ormeño <haroldmeneses2006@gmail.com>
2022-10-17 19:50:06 -0500

    Css basic design

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ n2 = document.querySelector(".n2");
 txtn1 = document.querySelector(".text_n1");
 txtn2 = document.querySelector(".text_n2");
 txtresult = document.querySelector(".text_result");
+sum_but = document.querySelector(".sumar_button");
+res_but = document.querySelector(".restar_button");
+mult_but = document.querySelector(".multiplicar_button");
+div_but = document.querySelector(".dividir_button");
+reset_but = document.querySelector(".button_reset");
+
+
+sum_but.addEventListener("click", Sumar);
+res_but.addEventListener("click", Restar);
+mult_but.addEventListener("click",Multiplicar);
+div_but.addEventListener("click", Dividir);
+reset_but.addEventListener("click", Reset);
+
 function WriteNum(){
     txtn1.innerText = n1.value;
     txtn2.innerText = n2.value;
@@ -30,7 +43,7 @@ function Dividir(){
 function Reset(){
     n1.value = "";
     n2.value = "";
-    txtn1.innerText = "";
-    txtn2.innerText = "";
-    txtresult.innerText = "";
+    txtn1.innerText = "000";
+    txtn2.innerText = "000";
+    txtresult.innerText = "000";
 }
