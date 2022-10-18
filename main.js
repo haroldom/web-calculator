c28c2978f3e4d36ee428fa468f9a6ca0a4ab53af
Harold Ormeño <haroldmeneses2006@gmail.com>
2022-10-18 18:55:19 -0500

    Calculator v.1.0

--- main.js
+++ main.js
@@ -16,6 +16,41 @@ mult_but.addEventListener("click",Multiplicar);
 div_but.addEventListener("click", Dividir);
 reset_but.addEventListener("click", Reset);
 
+function validationNum(){
+    let validation_condition;
+
+    if(n1.value=="" && n2.value==""){
+        validation_condition = 0;
+    } else if(n1.value!="" && n2.value == ""){
+        validation_condition = 1;
+    } else if(n1.value=="" && n2.value != ""){
+        validation_condition = 2;
+    } else{
+        validation_condition = 3;
+    }
+
+    switch(validation_condition){
+        case 0:
+            n1.classList.add("error");
+            n2.classList.add("error");
+            break
+        case 1:
+            n2.classList.add("error");
+            n1.classList.remove("error");
+            break
+        case 2:
+            n1.classList.add("error");
+            n2.classList.remove("error");
+            break
+        case 3:
+            n1.classList.remove("error");
+            n2.classList.remove("error");
+            return true
+    }
+
+
+}
+
 function WriteNum(){
     txtn1.innerText = n1.value;
     txtn2.innerText = n2.value;
@@ -23,24 +58,34 @@ function WriteNum(){
 
 
 function Sumar(){
-    WriteNum();
-    txtresult.innerText = parseInt(n1.value) + parseInt(n2.value)
+    if (validationNum()==true){
+        WriteNum();
+        txtresult.innerText = parseInt(n1.value) + parseInt(n2.value);
+    }
 }
 
 function Restar(){
-    WriteNum();
-    txtresult.innerText = parseInt(n1.value) - parseInt(n2.value)
+    if (validationNum()==true){
+        WriteNum();
+        txtresult.innerText = parseInt(n1.value) - parseInt(n2.value);
+    }
 }
 function Multiplicar(){
-    WriteNum();
-    txtresult.innerText = parseInt(n1.value) * parseInt(n2.value)
+    if (validationNum()==true){
+        WriteNum();
+        txtresult.innerText = parseInt(n1.value) * parseInt(n2.value);
+    }
 }
 function Dividir(){
-    WriteNum();
-    txtresult.innerText = parseInt(n1.value) / parseInt(n2.value)
+    if (validationNum()==true){
+        WriteNum();
+        txtresult.innerText = parseInt(n1.value) / parseInt(n2.value);
+    }
 }
 
 function Reset(){
+    n1.classList.remove("error");
+    n2.classList.remove("error");
     n1.value = "";
     n2.value = "";
     txtn1.innerText = "000";
